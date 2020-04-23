7cd7d725b65243ccba5809f46e85a8c4a86aa480
KseniGal <59790826+KseniGal@users.noreply.github.com>
2020-04-23 17:52:15 +0500

    newborn

---
+++ script.js
@@ -0,0 +1,14 @@
+jQuery(function($){
+	"use strict";
+
+	$('.reviews_slider').slick({
+		arrows: true,
+	});	
+});
+jQuery(function($){
+	"use strict";
+
+	$('.portfolio_slider').slick({
+		arrows: true,
+	});	
+});
